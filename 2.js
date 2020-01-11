f2be0076704baa82608e35234131e4cf425c2c15
Muhammad Rizky Aziz Rundono <55979870+azizkui@users.noreply.github.com>
2020-01-11 19:52:24 +0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,7 @@
+let huruf1 = ['T','S','Q','P','R'];
+let huruf2 = ['W','U','V'];
+let huruf3 = ['M','L','O','N'];
+
+console.log('',huruf2.sort(),'\n',huruf1.sort(),'\n'+'\n');
+console.log('',huruf2.sort(),'\n',huruf3.sort(),'\n',huruf2.sort());
+
